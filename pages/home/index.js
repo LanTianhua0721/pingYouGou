e8fbbbd372c8f654bdebd700d5b89bb09118bae2
蓝天化 <lth091611@outlook.com>
2019-10-17 11:06:20 +0800

    完成下拉刷新页面

--- pages/home/index.js
+++ pages/home/index.js
@@ -2,6 +2,7 @@
 
 //调用
 import ZhenxinRequest from '../../utils/request.js';
+
 Page({
 
   /**
@@ -14,8 +15,9 @@ Page({
     autoplay: true,
     duration: 2000,
     interval: 3000,
+    topFlag: true,
   },
-
+  
   /**
    * 生命周期函数--监听页面加载
    */
@@ -44,32 +46,32 @@ Page({
   //     }
   //   })
 
-    ZhenxinRequest('home/swiperdata').then(res=>{
-      // 打印数据
-      //console.log(res.data.message);
+    // ZhenxinRequest('home/swiperdata').then(res=>{
+    //   // 打印数据
+    //   //console.log(res.data.message);
       
-      this .setData({
-        swiperData: res.data.message
-      })
-    })
+    //   this .setData({
+    //     swiperData: res.data.message
+    //   })
+    // })
 
-    ZhenxinRequest('home/catitems').then(res => {
-      // 打印数据
-      //console.log(res.data.message);
+    // ZhenxinRequest('home/catitems').then(res => {
+    //   // 打印数据
+    //   //console.log(res.data.message);
 
-      this.setData({
-        catiData: res.data.message
-      })
-    })
+    //   this.setData({
+    //     catiData: res.data.message
+    //   })
+    // })
 
-    ZhenxinRequest('home/floordata').then(res => {
-      // 打印数据
-      //console.log(res.data.message);
+    // ZhenxinRequest('home/floordata').then(res => {
+    //   // 打印数据
+    //   //console.log(res.data.message);
 
-      this.setData({
-        floorData: res.data.message
-      })
-    })
+    //   this.setData({
+    //     floorData: res.data.message
+    //   })
+    // })
   },
 
   /**
@@ -99,12 +101,20 @@ Page({
   onUnload: function () {
 
   },
-
+ 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+      duration: 1500
+    }),
+    this.zhenxinSuccess();
+    setTimeout(function () {
+      wx.stopPullDownRefresh()
+    },1500);
   },
 
   /**
@@ -119,5 +129,45 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  // 页面滚动触发事件的处理函数
+  onPageScroll: function (a) {
+    this.setData({
+      topFlag: a.scrollTop < 200,
+    }); 
+  },
+  top: function(){
+    wx.pageScrollTo({
+      scrollTop: 0,
+    })
+  },
+  // 封装函数
+  zhenxinSuccess() {
+    ZhenxinRequest('home/swiperdata').then(res =>     {
+      // 打印数据
+      //console.log(res.data.message);
+
+      this.setData({
+        swiperData: res.data.message
+      })
+    })
+
+    ZhenxinRequest('home/catitems').then(res => {
+      // 打印数据
+      //console.log(res.data.message);
+
+      this.setData({
+        catiData: res.data.message
+      })
+    })
+
+    ZhenxinRequest('home/floordata').then(res => {
+      // 打印数据
+      //console.log(res.data.message);
+
+      this.setData({
+        floorData: res.data.message
+      })
+    })
   }
 })
