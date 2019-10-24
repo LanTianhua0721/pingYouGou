1cf699becf9a1bec85ccff12ce22d20b9e00fafd
蒙标 <biao19991023@163.com>
2019-10-24 11:58:16 +0800

    完成搜索检索

--- utils/request.js
+++ utils/request.js
@@ -1,10 +1,11 @@
 // 自定义函数
-const ZhenxinRequest = function(path){
+const ZhenxinRequest = function(path, data){
   //拼接urls
   let url = 'https://www.zhengzhicheng.cn/api/public/v1/' + path;
   return new Promise(function(resolve,reject){
     wx.request({
       url: url,
+      data: data,
       success: (res) => {
         // 把数据传给“成功解决”
         resolve(res);
