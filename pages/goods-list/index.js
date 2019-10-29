3fb87bc6f3c26a2eb80ea9503b4d46bc3208988f
臻薪 <lth091611@outlook.com>
2019-10-29 16:06:50 +0800

    完成添加搜索条

--- pages/goods-list/index.js
+++ pages/goods-list/index.js
@@ -1,18 +1,21 @@
-// components/search-bar/index.js
+// pages/goods-list/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    type: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(options.query);
+    this.setData({
+      type: options.query
+    })
   },
 
   /**
--- utils/request.js
+++ utils/request.js
@@ -1,7 +1,7 @@
 // 自定义函数
 const ZhenxinRequest = function(path, data){
   //拼接urls
-  let url = 'https://www.zhengzhicheng.cn/api/public/v1/' + path;
+  let url = 'https://www.ehomespace.com/api/public/v1/' + path;
   return new Promise(function(resolve,reject){
     wx.request({
       url: url,
